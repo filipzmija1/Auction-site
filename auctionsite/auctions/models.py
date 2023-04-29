1ab7777aa1ed9c3e6c7896917a4170bddebd069b
filipzmija1 <zmijafilip@gmail.com>
2023-04-29 12:17:16 +0200

    added models

--- auctionsite/auctions/models.py
+++ auctionsite/auctions/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Auction(models.Model):
+    name = models.CharField(max_length=255)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    min_price = models.DecimalField(max_digits=10, decimal_places=2)
+    buy_now_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    duration = models.DurationField()
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='seller')
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='buyer')
+
+
+class Bid(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    time = models.DateTimeField(auto_now_add=True)
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Opinion(models.Model):
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    comment = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
