f3d508225e1f8a8583acb13e1dd8c19b18365aa7
filipzmija1 <zmijafilip@gmail.com>
2023-06-01 22:22:40 +0200

    email host password delete

--- auctionsite/auctionsite/settings.py
+++ auctionsite/auctionsite/settings.py
@@ -165,7 +165,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'testdjangoapp381@gmail.com'
-EMAIL_HOST_PASSWORD = 'vqqz uaql kcvr xdxo'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
